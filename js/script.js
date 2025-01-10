7ffbc44d47dbd3fd1be6b9226d85d19643791968
jjeanssousa <jjeanssousa16@gmail.com>
2025-01-10 11:53:35 -0300

    criação da seção projetos e estilização para telas maiores, faltando apenas para telas menores.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+ // Inicializa o ScrollReveal
+ ScrollReveal().reveal('.section-home', {
+    distance: '50px',      // Distância do movimento
+    origin: 'bottom',      // Origem do movimento
+    duration: 1000,        // Duração da animação (em milissegundos)
+    easing: 'ease-in-out', // Tipo de transição
+    delay: 300,             // Atraso antes de começar (em milissegundos)
+    reset: true,
+    opacity: null
+});
+
+ScrollReveal().reveal('.section-sobre', {
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    easing: 'ease-in-out', // Tipo de transição
+    delay: 300,
+    reset: true,
+    opacity: null
+});
+
+ScrollReveal().reveal('.container-projeto', {
+    distance: '50px',
+    origin: 'left',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 300,
+    reset: true,
+    opacity: null
+});
